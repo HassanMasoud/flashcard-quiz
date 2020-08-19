1bfc83b3bfd96e66d25394a788285c3b8ab2942a
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-19 22:15:14 +0300

    Create sample questions

--- src/App.js
+++ src/App.js
@@ -4,4 +4,19 @@ function App() {
   return <h1>Hello world</h1>;
 }
 
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: "What is 1 + 1?",
+    answer: "1",
+    options: ["1", "2", "3", "4"],
+  },
+  {
+    id: 2,
+    question: "What is 2 + 2?",
+    answer: "4",
+    options: ["2", "3", "4", "5"],
+  },
+];
+
 export default App;
