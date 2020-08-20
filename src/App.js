e0130328e0aa4029396934b44eae15ef7d8f1d84
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-20 18:17:58 +0300

    Style card grid and card widths

--- src/App.js
+++ src/App.js
@@ -32,7 +32,11 @@ function App() {
     return textarea.value;
   }
 
-  return <FlashcardList flashcards={flashcards} />;
+  return (
+    <div className="container">
+      <FlashcardList flashcards={flashcards} />
+    </div>
+  );
 }
 
 const SAMPLE_FLASHCARDS = [
