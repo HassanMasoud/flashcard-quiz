96455cc2b181ab8432b48c92c1529cd98efd9b47
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-20 18:31:13 +0300

    Calculate card heights dynamically

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,13 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function Flashcard({ flashcard }) {
   const [flip, setFlip] = useState(false);
+  const [height, setHeight] = useState("inital");
+
+  const frontEl = useRef();
+  const backEl = useRef();
+
+  function setMaxHeight() {
+    const frontHeight = frontEl.current.getBoundingClientRect().height;
+    const backHeight = backEl.current.getBoundingClientRect().height;
+    setHeight(Math.max(frontHeight, backHeight, 100));
+  }
+
+  useEffect(setMaxHeight, [
+    flashcard.question,
+    flashcard.answer,
+    flashcard.options,
+  ]);
+
   return (
     <div
       onClick={() => setFlip(!flip)}
       className={`card ${flip ? "flip" : ""}`}
+      style={{ height: height }}
     >
-      <div className="front">
+      <div className="front" ref={frontEl}>
         {flashcard.question}
         <div className="flashcard-options">
           {flashcard.options.map((option) => {
@@ -15,7 +33,9 @@ export default function Flashcard({ flashcard }) {
           })}
         </div>
       </div>
-      <div className="back">{flashcard.answer}</div>
+      <div className="back" ref={backEl}>
+        {flashcard.answer}
+      </div>
     </div>
   );
 }
